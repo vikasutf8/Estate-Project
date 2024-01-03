8daa00477c044c1c116007c53380ccac5ca7f280
vikasutf8 <21bcs241@gamil.com>
2024-01-04 00:28:39 +0530

    create signinApi router

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,41 @@
 import { errorHandler } from '../Utils/error.js';
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-
-export const signup = async (req, res,next) => {
+export const signup = async (req, res, next) => {
     //    console.log(req.body)
     const { username, email, password } = req.body;
-    const hashedPassword =bcryptjs.hashSync(password,10)
-    const newUser = new User({ username, email, password:hashedPassword });
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ username, email, password: hashedPassword });
     try {
         await newUser.save()
-    res.status(201).json("User created successfully")
+        res.status(201).json("User created successfully")
     } catch (error) {
         // res.status(500).json(error.message)
         // next(errorHandler(550,"error from the function"))
         next(error)
     }
+}
 
-    
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body
+    try {
+        const validUser = await User.findOne({ email: email });
+        if (!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong Credentials'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+        // remove password
+        const {password :pass,...restInfo} =validUser._doc;
+
+        res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(restInfo);
+
+    } catch (error) {
+        next(error)
+    }
 }
